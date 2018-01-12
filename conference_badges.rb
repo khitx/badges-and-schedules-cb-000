1d6ac12c434ec533730483a4eefad7e3ba3913c4
Kathy Hudson <khitx@icloud.com>
2018-01-12 19:53:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,7 @@ end
 def batch_badge_creator(names)
   names.collect {|name| "Hello, my name is #{name}"}
 end
+
+def assign_rooms(names)
+  names.collect {|name| "Hello, #{name}! You'll be assigned to room "}  
+end
