1010e44e2df0cb18dfba62f01cc24a585ec4d9dc
Kathy Hudson <khitx@icloud.com>
2018-01-12 19:59:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,9 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  names.collect {|name| "Hello, #{name}! You'll be assigned to room "}
+  room_assgnmts = []
+  names.each_with_index {|name, index| room_assgnmts << "Hello, #{name}! You'll be assigned to room #{index + 1}."}
+  room_assgnmts
 end
+
+
